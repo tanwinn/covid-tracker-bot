33077a434daf50c0f47a6e80e578c35292ff6954
Thanh Nguyen <tnguyen19@wooster.edu>
2020-06-19 18:50:36 -0400

    Update privacy policy page

--- api/main.py
+++ api/main.py
@@ -3,10 +3,11 @@ api.main.py
 """
 import logging
 import os
+from pathlib import Path
 from typing import List
 
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 from wit import Wit
 
@@ -19,6 +20,8 @@ WIT_TOKEN = os.environ.get("WIT_TOKEN")
 FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN")
 # A user secret to verify webhook get request
 FB_VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
+
+PRIVACY_POLICY_PATH = Path(__file__).parent.parent / "resources" / "pp.txt"
 
 
 class Event(BaseModel):
@@ -45,17 +48,16 @@ def messenger_webhook(request: Request):
     verify_token = request.query_params.get("hub.verify_token")
     # check whether the verify tokens match
 
-    APP_LOGGER.info("testing info log")
-    print(f"verify_token: {verify_token}, envvar: {FB_VERIFY_TOKEN}")
-    APP_LOGGER.warning(f"LOG:: verify_token: {verify_token}, envvar: {FB_VERIFY_TOKEN}")
-
     if (
         verify_token == FB_VERIFY_TOKEN
         and request.query_params.get("hub.mode") == "subscribe"
     ):
         # respond with the challenge to confirm
-        resp = int(request.query_params.get("hub.challenge", "errored"))
-        APP_LOGGER.warning(f"Return challenge: {resp}")
+        try:
+            resp = int(request.query_params.get("hub.challenge", "errored"))
+        except ValueError:
+            resp = request.query_params.get("hub.challenge", "errored")
+        APP_LOGGER.debug(f"Return challenge: {type(resp)} {resp}")
         return JSONResponse(content=resp, headers={"Content-Type": "text/html"})
     APP_LOGGER.error(
         f"Invalid Request or Verification Token: given {verify_token}, expected {FB_VERIFY_TOKEN}"
@@ -68,20 +70,11 @@ def messenger_post(data: Event):  # pylint: disable=unused-argument
     """
     Handler for webhook (currently for postback and messages)
     """
-    # if data.object == "page":
-    #     # boilerplate code
-    #     for entry in data.entry:
-    #         messages = entry["messaging"]
-    #         if messages[0]:
-    #             # Get the first message
-    #             message = messages[0]
-    #             # Yay! We got a new message!
-    #             # We retrieve the Facebook user ID of the sender
-    #             fb_id = message["sender"]["id"]
-    #             # We retrieve the message content
-    #             text = message["message"]["text"]
-    #             # Let's forward the message to Wit /message
-    #             # and customize our response to the message in handle_message
-    #             response = client.message(msg=text, context={"session_id": fb_id})
-    #             handle_message(response=response, fb_id=fb_id)
     return "dummy"
+
+
+@app.get("/privacy-policy", response_class=HTMLResponse)
+def get_privacy_policy():
+    with open(PRIVACY_POLICY_PATH) as rfile:
+        return rfile.read()
+    return None
--- tests/test_api.py
+++ tests/test_api.py
@@ -19,13 +19,22 @@ def test_webhook_always_return_200(api_client, params):
     assert api_client.get(f"/webhook?{params}").status_code == 200
 
 
-@pytest.mark.parametrize("params", ["hub.verify_token=invalid", "hub.verify_token=foo"])
-def test_get_webhook_failed(api_client, params):
+@pytest.mark.parametrize(
+    "params",
+    [
+        "hub.verify_token=invalid&hub.mode=subscribe",
+        "hub.verify_token=foo",
+        "hub.verify_token=foobar&hub.mode=unsubscribe",
+    ],
+)
+def test_get_webhook_failed(api_client, params, monkeypatch):
+    monkeypatch.setattr(api.main, "FB_VERIFY_TOKEN", value="foobar")
     response = api_client.get(f"/webhook?{params}")
     assert response.json() == "Invalid Request or Verification Token"
+    monkeypatch.undo()
 
 
-@pytest.mark.parametrize("token,challenge", [("foo", "123"), ("flamingo", "ant")])
+@pytest.mark.parametrize("token,challenge", [("foo", 123), ("flamingo", "ant")])
 def test_get_webhook_succeeded(api_client, token, challenge, monkeypatch):
     monkeypatch.setattr(api.main, "FB_VERIFY_TOKEN", value=token)
     response = api_client.get(
@@ -33,3 +42,7 @@ def test_get_webhook_succeeded(api_client, token, challenge, monkeypatch):
     )
     assert response.json() == challenge
     monkeypatch.undo()
+
+
+def test_get_privacy_policy(api_client):
+    assert api_client.get("/privacy-policy").status_code == 200
