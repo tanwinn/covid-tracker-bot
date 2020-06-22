bf6bd0601c3b046e01950183acbbd53adba830f6
Thanh Nguyen <tnguyen19@wooster.edu>
2020-06-21 23:03:43 -0400

    Add log

--- api/main.py
+++ api/main.py
@@ -63,6 +63,7 @@ def messenger_post(data: facebook.Event):
     """
     Handler for webhook (currently for postback and messages)
     """
+    APP_LOGGER.warning(f"Data event:\n {data}")
     for entry in data.entry:
         # get all the messages
         messages = entry.messaging
